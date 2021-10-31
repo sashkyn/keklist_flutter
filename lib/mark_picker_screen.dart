664f195f95a6cc84b0139aaff9d74bb20d9c8581
Aleksandr Martsenyuk <sashkn2@gmail.com>
2021-10-31 13:37:33 +0300

    MarkPickerScreen: implement search

--- lib/mark_picker_screen.dart
+++ lib/mark_picker_screen.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:awesome_emojis/emoji.dart';
 import 'package:emarko/typealiases.dart';
 import 'package:flutter/material.dart';
 
@@ -11,30 +14,75 @@ class MarkPickerScreen extends StatefulWidget {
 }
 
 class _MarkPickerScreenState extends State<MarkPickerScreen> {
-  final List<String> marks = ["🔫", "🏃", "💩", "🤖", "💪", "☕", "✨", "👩", "💦", "🇻🇬", "✅"];
+  final List<Emoji> _usedMarks = Emoji.byKeyword('love').toList();
+
+  final List<Emoji> _marks = Emoji.all();
+
+  String _searchText = '';
+
+  List<Emoji> _filteredMarks = [];
+
+  List<Emoji> get _displayedMarks => _searchText.isEmpty ? _marks : _filteredMarks;
+
+  final TextEditingController _textEditingController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _textEditingController.addListener(() {
+      setState(() {
+        _searchText = _textEditingController.text;
+        _filteredMarks = _marks.where((mark) => mark.keywords.join().contains(_searchText)).toList();
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    final List<Widget> widgets = marks.map((item) {
-      return GestureDetector(
-        onTap: () {
-          var mark = CreationMark(item, '');
-          widget.onSelect(mark);
-          Navigator.pop(context);
-        },
-        child: Center(
-          child: Text(
-            item,
-            style: const TextStyle(fontSize: 55),
+    return Column(
+      children: [
+        TextField(
+          controller: _textEditingController,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'Search mark',
           ),
         ),
-      );
-    }).toList();
+        // Flexible(
+        //   child: GridView.count(
+        //     shrinkWrap: true,
+        //     crossAxisCount: 5,
+        //     children: _usedMarks.map((mark) => _makeMarkWidget(mark.char)).toList(),
+        //   ),
+        // ),
+        Flexible(
+          child: GridView.builder(
+            padding: EdgeInsets.all(8),
+            itemCount: _displayedMarks.length,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 5),
+            itemBuilder: (BuildContext context, int index) {
+              return _makeMarkWidget(_displayedMarks[index].char);
+            },
+          ),
+        ),
+      ],
+    );
+  }
 
-    return GridView.count(
-      padding: const EdgeInsets.all(8),
-      crossAxisCount: 5,
-      children: widgets,
+  Widget _makeMarkWidget(String item) {
+    return GestureDetector(
+      onTap: () {
+        var mark = CreationMark(item, '');
+        widget.onSelect(mark);
+        Navigator.pop(context);
+      },
+      child: Center(
+        child: Text(
+          item,
+          style: const TextStyle(fontSize: 55),
+        ),
+      ),
     );
   }
 }
