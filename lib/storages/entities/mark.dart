27836a2282d4dd1b4e3a009906f849ca9c034356
Aleksandr Martsenyuk <sashkn2@gmail.com>
2021-11-07 17:21:39 +0300

    SettingsScreen: add login and logout

--- lib/storages/entities/mark.dart
+++ lib/storages/entities/mark.dart
@@ -3,26 +3,31 @@ class Mark {
   final String emoji;
   final String note;
   final int dayIndex;
+  final int creationDate;
+  final int sortIndex;
 
   Mark({
     required this.uuid,
     required this.note,
     required this.emoji,
     required this.dayIndex,
+    required this.creationDate,
+    required this.sortIndex,
   });
 
   Mark.fromJson(Map<String, dynamic> json)
       : uuid = json['id'],
         emoji = json['emoji'],
         dayIndex = json['day_index'],
-        note = json['note'];
+        note = json['note'],
+        creationDate = json['creation_date'] ?? 0,
+        sortIndex = json["sort_index"] ?? 0;
 
-  Map<String, dynamic> toJson() {
-    return {
-      'id': uuid,
-      'emoji': emoji,
-      'note': note,
-      'day_index': dayIndex,
-    };
-  }
+  Map<String, dynamic> toJson() => {
+        'emoji': emoji,
+        'note': note,
+        'day_index': dayIndex,
+        'sort_index': sortIndex,
+        'creation_date': creationDate,
+      };
 }
--- lib/storages/firebase_storage.dart
+++ lib/storages/firebase_storage.dart
@@ -1,25 +1,44 @@
+// ignore_for_file: avoid_print
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:keklist/storages/entities/mark.dart';
 import 'package:keklist/storages/storage.dart';
 
 class FirebaseStorage extends Storage {
+  final String _stand;
+
   final DatabaseReference _databaseReference =
       FirebaseDatabase(databaseURL: 'https://keklist-881d8-default-rtdb.europe-west1.firebasedatabase.app/')
           .reference();
 
+  FirebaseStorage(this._stand);
+
   @override
   Future<void> addMark(Mark mark) async {
-    final map = <String, dynamic>{};
-    map['day_index'] = mark.dayIndex;
-    map['emoji'] = mark.emoji;
-    map['note'] = mark.note;
-    return _databaseReference.child('debug').child('sashkyn').child("marks").child(mark.uuid).set(map);
+    final userId = FirebaseAuth.instance.currentUser?.uid;
+    if (userId == null) {
+      print('userId is null!');
+      return;
+    }
+
+    // final map = <String, dynamic>{};
+    // map['day_index'] = mark.dayIndex;
+    // map['emoji'] = mark.emoji;
+    // map['note'] = mark.note;
+    return _databaseReference.child(_stand).child(userId).child('marks').child(mark.uuid).set(mark.toJson());
   }
 
   @override
   Future<List<Mark>> getMarks() async {
+    final userId = FirebaseAuth.instance.currentUser?.uid;
+    if (userId == null) {
+      print('userId is null!');
+      return [];
+    }
+
     final List<Mark> marks = [];
-    await _databaseReference.child("debug").child("sashkyn").child("marks").orderByKey().once().then((snapshot) {
+    await _databaseReference.child(_stand).child(userId).child('marks').once().then((snapshot) {
       snapshot.value.forEach((key, values) {
         final markMap = Map<String, dynamic>.from(values);
         markMap['id'] = key;
@@ -30,7 +49,12 @@ class FirebaseStorage extends Storage {
   }
 
   @override
-  Future<void> removeMarkFromDay(String id) {
-    return _databaseReference.child("debug").child("sashkyn").child("marks").child(id).remove();
+  Future<void> removeMarkFromDay(String id) async {
+    final userId = FirebaseAuth.instance.currentUser?.uid;
+    if (userId == null) {
+      print('userId is null!');
+      return;
+    }
+    return _databaseReference.child(_stand).child(userId).child('marks').child(id).remove();
   }
 }
