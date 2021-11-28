f031a50fbacf888cae91f9655206942229016a32
Aleksandr Martsenyuk <sashkn2@gmail.com>
2021-11-28 16:41:32 +0300

    Start implementing bloc

---
+++ lib/blocs/emoji_fetcher_bloc/emoji_fetcher_bloc.dart
@@ -0,0 +1,25 @@
+import 'package:bloc/bloc.dart';
+import 'package:emojis/emoji.dart';
+import 'package:meta/meta.dart';
+
+part 'emoji_fetcher_event.dart';
+part 'emoji_fetcher_state.dart';
+
+class EmojiFetcherBloc extends Bloc<EmojiFetcherEvent, EmojiFetcherState> {
+  final List<Emoji> _emojiList = Emoji.all();
+
+  EmojiFetcherBloc(EmojiFetcherState initialState)
+      : super(EmojiFetcherState('', Emoji.all().map((emoji) => emoji.char).toList())) {
+    on<SearchEmojiFetcherEvent>((event, emit) {
+      emit(
+        EmojiFetcherState(
+          event.searchtext,
+          _emojiList
+              .where((emoji) => emoji.keywords.join().contains(event.searchtext))
+              .map((emoji) => emoji.char)
+              .toList(),
+        ),
+      );
+    });
+  }
+}
---
+++ lib/blocs/mark_bloc/mark_state.dart
@@ -0,0 +1,6 @@
+part of 'mark_bloc.dart';
+
+@immutable
+abstract class MarkState {}
+
+class MarkInitial extends MarkState {}
