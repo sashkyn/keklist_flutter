a540a4c8506a21e4fd8a15c3ef1d250eec4d0de1
Aleksandr Martseniuk <sashkn2@gmail.com>
2023-02-12 22:05:08 +0530

    Add unit test for sorting list

---
+++ test/my_sorting_test.dart
@@ -0,0 +1,54 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:zenmode/blocs/mind_bloc/mind_bloc.dart';
+import 'package:zenmode/services/entities/mind.dart';
+
+void main() {
+  test(
+    'mySortedBy: sorted correctly',
+    () {
+      final List<Mind> unsortedMinds = [];
+      unsortedMinds.add(
+        const Mind(
+          id: '1',
+          note: 'Heh1',
+          emoji: ' ',
+          dayIndex: 0,
+          sortIndex: 5,
+          creationDate: 0,
+        ),
+      );
+      unsortedMinds.add(
+        const Mind(
+          id: '1',
+          note: 'Heh3',
+          emoji: ' ',
+          dayIndex: 0,
+          sortIndex: 20,
+          creationDate: 0,
+        ),
+      );
+      unsortedMinds.add(
+        const Mind(
+          id: '1',
+          note: 'Heh2',
+          emoji: ' ',
+          dayIndex: 0,
+          sortIndex: 10,
+          creationDate: 0,
+        ),
+      );
+
+      expect(
+        unsortedMinds.map((e) => e.sortIndex),
+        [5, 20, 10],
+      );
+
+      final List<Mind> sortedList = unsortedMinds.mySortedBy((it) => it.sortIndex).toList();
+
+      expect(
+        sortedList.map((e) => e.sortIndex),
+        [5, 10, 20],
+      );
+    },
+  );
+}
