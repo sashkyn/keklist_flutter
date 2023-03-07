1607f2a31e9a9e7d036d9f6c2bde82965b29273c
Aleksandr Martsenyuk <sashkn2@gmail.com>
2023-03-07 12:59:34 +0530

    Add AppleWatch Target like companion

---
+++ ios/zenwatch Watch App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  zenwatch Watch App
+//
+//  Created by Aleksandr Martseniuk on 07.03.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ios/zenwatch Watch App/zenwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  zenwatchApp.swift
+//  zenwatch Watch App
+//
+//  Created by Aleksandr Martseniuk on 07.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct zenwatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
