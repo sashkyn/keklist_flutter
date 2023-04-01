1e90da125a9748c7b384a7291424eab4d33ceebd
Aleksandr Martsenyuk <sashkn2@gmail.com>
2023-04-01 09:01:42 +0530

    Remove comment

--- ios/WatchApp/ui/helpers/NavigationLazyView.swift
+++ ios/WatchApp/ui/helpers/NavigationLazyView.swift
@@ -3,7 +3,6 @@ import SwiftUI
 struct NavigationLazyView<Content: View>: View {
     let build: () -> Content
     
-    // TODO: Почитать про @autoclosure
     init(_ build: @autoclosure @escaping () -> Content) {
         self.build = build
     }
