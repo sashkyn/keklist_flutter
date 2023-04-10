c3669faeec85884b917f51b69203fde1167b6f2b
Aleksandr Martsenyuk <sashkn2@gmail.com>
2023-04-10 13:29:12 +0530

    Fix

--- lib/blocs/auth_bloc/auth_state.dart
+++ lib/blocs/auth_bloc/auth_state.dart
@@ -1,34 +1,19 @@
 part of 'auth_bloc.dart';
 
-abstract class AuthState extends Equatable {
+abstract class AuthState {
   const AuthState();
 }
 
-class AuthInitial extends AuthState {
-  @override
-  List<Object?> get props => [];
-}
+class AuthInitial extends AuthState {}
 
-class AuthLoggedIn extends AuthState {
-  @override
-  List<Object?> get props => [];
-}
+class AuthLoggedIn extends AuthState {}
 
-class AuthLogouted extends AuthState {
-  @override
-  List<Object?> get props => [];
-}
+class AuthLogouted extends AuthState {}
 
-class AuthUserDeletedHimself extends AuthState {
-  @override
-  List<Object?> get props => [];
-}
+class AuthUserDeletedHimself extends AuthState {}
 
 class AuthCurrentStatus extends AuthState {
   final bool isLoggedIn;
-
-  @override
-  List<Object?> get props => [isLoggedIn];
 
   const AuthCurrentStatus({required this.isLoggedIn});
 }
