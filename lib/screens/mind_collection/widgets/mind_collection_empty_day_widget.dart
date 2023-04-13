c292ac267af87da14aeb804f3b9db70a75c2858a
Aleksandr Martsenyuk <sashkn2@gmail.com>
2023-04-13 14:05:11 +0530

    Made empty states

---
+++ lib/screens/mind_collection/widgets/mind_collection_empty_day_widget.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:rememoji/widgets/mind_widget.dart';
+
+class MindCollectionEmptyDayWidget extends StatelessWidget {
+  final String emoji;
+  final String text;
+
+  const MindCollectionEmptyDayWidget({
+    super.key,
+    required this.emoji,
+    required this.text,
+  });
+
+  factory MindCollectionEmptyDayWidget.past() {
+    return const MindCollectionEmptyDayWidget(
+      emoji: '📜',
+      text: 'Yesterday is history',
+    );
+  }
+
+  factory MindCollectionEmptyDayWidget.present() {
+    return const MindCollectionEmptyDayWidget(
+      emoji: '⏳',
+      text: 'Now',
+    );
+  }
+
+  factory MindCollectionEmptyDayWidget.future() {
+    return const MindCollectionEmptyDayWidget(
+      emoji: '🔮',
+      text: 'Tomorrow is a mystery',
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(height: 16.0),
+        MindWidget.sized(
+          item: emoji,
+          size: MindSize.large,
+          isHighlighted: false,
+        ),
+        const SizedBox(height: 8),
+        Text(
+          text,
+          style: TextStyle(color: Colors.black.withOpacity(0.3)),
+        ),
+        const SizedBox(height: 16.0),
+      ],
+    );
+  }
+}
