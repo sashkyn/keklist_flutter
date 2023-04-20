86da09996293581c8c41be9a69f19d153070bc67
Aleksandr Martsenyuk <sashkn2@gmail.com>
2023-04-20 15:44:26 +0530

    Made hiding keyboard by pan on search items

--- lib/screens/mind_collection/widgets/mind_search_result_widget.dart
+++ lib/screens/mind_collection/widgets/mind_search_result_widget.dart
@@ -3,42 +3,47 @@ import 'package:rememoji/screens/mind_day_collection/widgets/messaged_list/mind_
 import 'package:rememoji/services/entities/mind.dart';
 
 class MindSearchResultListWidget extends StatelessWidget {
+  final VoidCallback onPanDown;
   final List<Mind> results;
 
   const MindSearchResultListWidget({
     super.key,
     required this.results,
+    required this.onPanDown,
   });
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: results.length + 1,
-      itemBuilder: (context, index) {
-        if (index == 0) {
-          if (results.isEmpty) {
-            return const SizedBox.shrink();
-          }
-          return Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 16.0,
-            ),
-            child: Text(
-              'Found ${results.length} minds:',
-              style: const TextStyle(
-                color: Colors.black87,
+    return GestureDetector(
+      onPanDown: (_) => onPanDown(),
+      child: ListView.builder(
+        itemCount: results.length + 1,
+        itemBuilder: (context, index) {
+          if (index == 0) {
+            if (results.isEmpty) {
+              return const SizedBox.shrink();
+            }
+            return Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 16.0,
               ),
-            ),
-          );
-        } else {
-          if (results.isEmpty) {
-            return const SizedBox.shrink();
+              child: Text(
+                'Found ${results.length} minds:',
+                style: const TextStyle(
+                  color: Colors.black87,
+                ),
+              ),
+            );
           } else {
-            final Mind mind = results[index - 1];
-            return MindMessageWidget(mind: mind);
+            if (results.isEmpty) {
+              return const SizedBox.shrink();
+            } else {
+              final Mind mind = results[index - 1];
+              return MindMessageWidget(mind: mind);
+            }
           }
-        }
-      },
+        },
+      ),
     );
   }
 }
