88f7d7adeeeb65a638fb962dd0df3e029056b0a9
Aleksandr Martsenyuk <sashkn2@gmail.com>
2023-04-24 12:31:07 +0530

    Implement calendar

--- lib/helpers/mind_utils.dart
+++ lib/helpers/mind_utils.dart
@@ -1,3 +1,5 @@
+import 'package:rememoji/services/entities/mind.dart';
+
 class MindUtils {
   static const int millisecondsInDay = 1000 * 60 * 60 * 24;
 
@@ -5,4 +7,20 @@ class MindUtils {
       (from.millisecondsSinceEpoch + from.timeZoneOffset.inMilliseconds) ~/ millisecondsInDay;
 
   static DateTime getDateFromIndex(int index) => DateTime.fromMillisecondsSinceEpoch(millisecondsInDay * index);
+
+  static List<Mind> findMindsByDayIndex({
+    required int dayIndex,
+    required Iterable<Mind> allMinds,
+  }) {
+    return allMinds.where((item) => dayIndex == item.dayIndex).mySortedBy((it) => it.sortIndex).toList();
+  }
+}
+
+// NOTE: Sorted by.
+
+extension ListIterable<E> on Iterable<E> {
+  Iterable<E> mySortedBy(Comparable Function(E e) key) => toList()
+    ..sort(
+      (a, b) => key(a).compareTo(key(b)),
+    );
 }
