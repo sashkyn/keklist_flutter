8d666da9232c6a3eef7d12a9a716e33544cfe952
Aleksandr Martsenyuk <sashkn2@gmail.com>
2023-05-09 10:50:53 +0530

    Made toggle for Offline mode with saving state in Shared Prefs

--- lib/blocs/settings_bloc/settings_state.dart
+++ lib/blocs/settings_bloc/settings_state.dart
@@ -3,9 +3,23 @@ part of 'settings_bloc.dart';
 class SettingsState {
   final bool isMindContentVisible;
   final bool needToShowWhatsNewOnStart;
+  final bool isOfflineMode;
 
   const SettingsState({
     required this.isMindContentVisible,
     required this.needToShowWhatsNewOnStart,
+    required this.isOfflineMode,
   });
+
+  SettingsState copyWith({
+    bool? isMindContentVisible,
+    bool? needToShowWhatsNewOnStart,
+    bool? isOfflineMode,
+  }) {
+    return SettingsState(
+      isMindContentVisible: isMindContentVisible ?? this.isMindContentVisible,
+      needToShowWhatsNewOnStart: needToShowWhatsNewOnStart ?? this.needToShowWhatsNewOnStart,
+      isOfflineMode: isOfflineMode ?? this.isOfflineMode,
+    );
+  }
 }
