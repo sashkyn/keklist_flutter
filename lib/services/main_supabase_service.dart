f9c83e4802b603d972d87957c7c6a5c68b991e1f
Aleksandr Martsenyuk <sashkn2@gmail.com>
2023-05-10 11:57:06 +0530

    Started to parse creation_date

--- lib/services/main_supabase_service.dart
+++ lib/services/main_supabase_service.dart
@@ -6,24 +6,15 @@ import 'package:rememoji/services/main_service.dart';
 
 class MainSupabaseService implements MainService {
   final _client = Supabase.instance.client;
-  late final Set<Mind> _cachedMinds = {};
 
   @override
   FutureOr<Iterable<Mind>> getMindList() async {
-    if (_cachedMinds.isNotEmpty) {
-      return _cachedMinds;
-    }
-
     if (_client.auth.currentUser == null) {
       return Future.error('You did not auth to Supabase');
     }
 
     final List<dynamic> listOfEntities = await _client.from('minds').select();
     final List<Mind> minds = listOfEntities.map((e) => Mind.fromSupabaseJson(e)).toList();
-
-    _cachedMinds
-      ..clear()
-      ..addAll(minds);
 
     return minds;
   }
@@ -34,11 +25,7 @@ class MainSupabaseService implements MainService {
       return Future.error('You did not auth to Supabase');
     }
 
-    await _client.from('minds').insert(
-          mark.toSupabaseJson(userId: _client.auth.currentUser!.id),
-        );
-
-    _cachedMinds.add(mark);
+    await _client.from('minds').insert(mark.toSupabaseJson(userId: _client.auth.currentUser!.id,));
   }
 
   @override
@@ -48,15 +35,17 @@ class MainSupabaseService implements MainService {
     }
 
     await _client.from('minds').delete().eq('uuid', id);
-
-    _cachedMinds.removeWhere((element) => element.id == id);
   }
 
   @override
-  FutureOr<void> addAll({required List<Mind> list}) async {
-    await Future.forEach(list, (Mind element) async {
-      await addMind(element);
-    });
+  FutureOr<void> addAllMinds({required List<Mind> list}) async {
+    if (_client.auth.currentUser == null) {
+      return Future.error('You did not auth to Supabase');
+    }
+
+    final Iterable<Map<String, dynamic>> listOfEntries =
+        list.map((e) => e.toSupabaseJson(userId: _client.auth.currentUser!.id));
+    await _client.from('minds').insert(listOfEntries);
   }
 
   @override
@@ -66,18 +55,6 @@ class MainSupabaseService implements MainService {
       return Future.error('Trying to delete admin account!');
     }
     return await _client.rpc('deleteUser');
-  }
-
-  @override
-  FutureOr<void> editMindNote({
-    required String mindId,
-    required String newNote,
-  }) async {
-    if (_client.auth.currentUser == null) {
-      return Future.error('You did not auth to Supabase');
-    }
-
-    return await _client.from('minds').update({'note': newNote}).eq('uuid', mindId);
   }
 
   @override
@@ -93,30 +70,11 @@ class MainSupabaseService implements MainService {
   }
 
   @override
-  FutureOr<void> editMindEmoji({
-    required String mindId,
-    required String newEmoji,
-  }) async {
-    if (_client.auth.currentUser == null) {
-      return Future.error('You did not auth to Supabase');
-    }
-
-    return await _client.from('minds').update({'emoji': newEmoji}).eq('uuid', mindId);
-  }
-
-  @override
-  FutureOr<void> clearTempCache() {
-    // Очищаем закешированные данные.
-    _cachedMinds.clear();
-  }
-
-  @override
   FutureOr<void> deleteAllMinds() async {
     if (_client.auth.currentUser == null) {
       return Future.error('You did not auth to Supabase');
     }
 
     await _client.from('minds').delete().eq('user_id', _client.auth.currentUser!.id);
-    _cachedMinds.clear();
   }
 }
