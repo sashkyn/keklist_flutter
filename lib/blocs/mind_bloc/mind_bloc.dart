f1cc7b1b67eb3f2ef038ca46cd552bb93c87087e
Aleksandr Martsenyuk <sashkn2@gmail.com>
2023-11-13 16:02:24 +0100

    Fix lagging

--- lib/blocs/mind_bloc/mind_bloc.dart
+++ lib/blocs/mind_bloc/mind_bloc.dart
@@ -61,12 +61,13 @@ final class MindBloc extends Bloc<MindEvent, MindState> with DisposeBag {
     on<MindStopSearch>(_stopSearch);
     on<MindEnterSearchText>(
       _enterTextSearch,
-      transformer: (events, mapper) => events.debounceTime(const Duration(milliseconds: 100)).asyncExpand(mapper),
+      transformer: (events, mapper) => events.debounceTime(const Duration(milliseconds: 300)).asyncExpand(mapper),
     );
     on<MindUploadCandidates>(_uploadCandidates);
+    // INFO: 500 мс для того чтобы не лагало и клавиатура успела скрыться
     on<MindChangeCreateText>(
       _changeTextOfCreatingMind,
-      transformer: (events, mapper) => events.debounceTime(const Duration(milliseconds: 100)).asyncExpand(mapper),
+      transformer: (events, mapper) => events.debounceTime(const Duration(milliseconds: 500)).asyncExpand(mapper),
     );
     on<MindGetUploadCandidates>(_getUploadCandidates);
     on<MindInternalGetListFromCache>(
