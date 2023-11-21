e34dcbca8d3834d7e7cb57901e540debfa1cf412
Aleksandr Martsenyuk <sashkn2@gmail.com>
2023-11-21 17:45:36 +0100

    Upgrade major deps

--- lib/blocs/auth_bloc/auth_bloc.dart
+++ lib/blocs/auth_bloc/auth_bloc.dart
@@ -93,7 +93,9 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> with DisposeBag {
     final String webResult = await FlutterWebAuth2.authenticate(
       url: result.url.toString(),
       callbackUrlScheme: 'io.supabase.points',
-      preferEphemeral: false,
+      options: const FlutterWebAuth2Options(
+        preferEphemeral: false,
+      ),
     );
 
     final Uri uri = Uri.parse(webResult);
